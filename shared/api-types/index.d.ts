5aeb0fd2e659c69a49f44f47182d8a5ea7f06185
Phanty133 <48225206+Phanty133@users.noreply.github.com>
2022-09-18 22:30:17 +0200

    Shared API types

---
+++ shared/api-types/index.d.ts
@@ -0,0 +1,169 @@
+import type { ActionSubtype, ActionType, Bounds, MistakeChild, MistakeHash, MistakeSubtype, MistakeType } from "@shared/diff-engine";
+
+export type UUID = string;
+export type SubmissionID = string;
+
+export enum SubmissionState {
+	UNGRADED = "UNGRADED",
+	REJECTED = "REJECTED",
+	WIP = "WIP",
+	DONE = "DONE"
+}
+
+export enum SettingsType {
+	STRING = "STRING",
+	NUMBER = "NUMBER",
+	NESTED = "NESTED"
+}
+
+export enum UserRole {
+	NONE = "NONE",
+	EDITOR = "EDITOR",
+	ADMIN = "ADMIN"
+}
+
+export enum RegisterUpdateEventType {
+	ADD = "ADD",
+	EDIT = "EDIT",
+	DELETE = "DELETE"
+}
+
+export interface Workspace {
+	id: UUID,
+	name: string,
+	template: string,
+	submissions: Record<SubmissionID, Submission>,
+	register: RegisterEntry[]
+}
+
+export interface Submission {
+	id: SubmissionID,
+	state: SubmissionState,
+	data: {
+		text: string,
+		ignoreText: Bounds[],
+		mistakes: MistakeData[],
+		metadata: {
+			age: number,
+			language: string,
+			language_other: string,
+			level: string,
+			degree: string,
+			country: string,
+			city: string
+		}
+	} | null
+}
+
+export interface MistakeData {
+	id: UUID,
+	hash: MistakeHash
+	type: MistakeType,
+	subtype: MistakeSubtype,
+	actions: ActionData[],
+	boundsCheck: Bounds,
+	boundsCorrect: Bounds,
+	boundsDiff: Bounds,
+	word: string,
+	wordCorrect?: string,
+	registerId: string | null,
+	children: MistakeChild[]
+}
+
+export interface ActionData {
+	id: UUID,
+	type: ActionType,
+	subtype: ActionSubtype,
+	indexCheck: number,
+	indexCorrect: number,
+	indexDiff?: number,
+	char: string
+}
+
+export interface RegisterEntry {
+	id: UUID,
+	mistakes: MistakeHash[],
+	description: string,
+	ignore: boolean,
+	count: number
+}
+
+export interface Setting {
+	id: UUID,
+	key: string,
+	name: string,
+	type: SettingsType,
+	value: any,
+	min?: number,
+	max?: number,
+}
+
+export interface User {
+	user: string,
+	pass: string,
+	role: UserRole,
+}
+
+export interface RequestSubmissionEventData {
+	id: SubmissionID
+}
+
+export interface RegisterNewEventData {
+	data: RegisterEntry
+}
+
+export interface RegisterEditEventData {
+	id: UUID,
+	data: RegisterEntry
+}
+
+export interface RegisterDeleteEventData {
+	id: UUID
+}
+
+export interface MistakeMergeEventData {
+	mistakes: MistakeHash[]
+}
+
+export interface MistakeUnmergeEventData {
+	mistake: MistakeHash
+}
+
+export interface TextIgnoreEventData {
+	ignoreBounds: Bounds[]
+}
+
+export interface SubmissionStateChangeEventData {
+	id: SubmissionID,
+	state: SubmissionState
+}
+
+export interface SubmissionDataEventData {
+	data: SubmissionData
+}
+
+export interface RegisterUpdateEventData {
+	id: UUID,
+	data: RegisterEntry,
+	type: RegisterUpdateEventType
+}
+
+export interface SubmissionRegenEventData {
+	ids: SubmissionID[]
+}
+
+export interface SubmissionData {
+	id: SubmissionID,
+	text: string,
+	ignoreText: Bounds[],
+	mistakes: MistakeData[],
+	metadata: {
+		age: number,
+		language: string,
+		language_other: string,
+		level: string,
+		degree: string,
+		country: string,
+		city: string
+	}
+}
