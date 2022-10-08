6c31820e9c88136727fc87b560e90fd6329d2e5f
tower <towergame.epic@gmail.com>
2022-10-08 22:12:16 +0300

    Add case for really small percentages

--- shared/visualizer/visualizerEvents.js
+++ shared/visualizer/visualizerEvents.js
@@ -4,11 +4,12 @@ let timeout = null;
 
 function setTooltipText(description, count, percentage) {
   const tooltip = document.getElementById("tooltip");
+  const percent = Math.floor(percentage * 1000) / 10;
   tooltip.getElementsByClassName("desc")[0].innerHTML = description;
   tooltip.getElementsByClassName(
     "footer"
   )[0].innerHTML = `Kļūda fiksēta ${count} (${
-    Math.floor(percentage * 1000) / 10
+    percent >= 0.1 ? percent : "<0.1"
   }%) darbos`;
 }
 
