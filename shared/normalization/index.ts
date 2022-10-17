b7d9ffa64d40537ea060f834b0bf0dd3c2263112
tower <towergame.epic@gmail.com>
2022-10-18 01:55:59 +0300

    Normalisation updates

--- shared/normalization/index.ts
+++ shared/normalization/index.ts
@@ -2,6 +2,8 @@ export function processString(text: string) {
 	return text
 		// Debug
 		// .replace(/[A-z]/g, "^")
+		// Quotation marks
+		.replace(/['`´<>“”‘’«»‟‹›„]/g, "\"")
 		// Clean up whitespaces
 		.replace(/\n\s+/g, "\n") // remove any whitespace after newlines
 		.replace(/\s+\n/g, "\n") // remove any spaces before newlines
@@ -10,17 +12,21 @@ export function processString(text: string) {
 		// Ellipses
 		.replace(/\.[ ]?\.[ ]?\.[ ]?/g, "…")
 		// Add space to punctuation
-		.replace(/\.(?=[^\s])/g, ". ") // Add space after dot, if none
-		.replace(/,(?=[^\s\"])/g, ", ") // Add space after coma, if none
+		.replace(/((?<!"[^"]*?)\.(?![\.\s]?$))|(\.(?![\.\s\"]?))|((?<=^[^"]*)\.(?![\.\s]?$))/gm, ". ") // Add space after dot, if none
+		.replace(/((?<!"[^"]*?),(?![\s]?$))|(,(?![\s\"]?))|((?<=^[^"]*),(?![\s]?$))/gm, ", ") // Add space after coma, if none
+		.replace(/((?<!"[^"]*?)\?(?![\s]?$))|(\?(?![\s\"]?))|((?<=^[^"]*)\?(?![\s]?$))/gm, "? ") // Add space after question mark, if none
+		.replace(/((?<!"[^"]*?)!(?![\s]?$))|(!(?![\s\"]?))|((?<=^[^"]*)!(?![\s]?$))/gm, "! ") // Add space after exclamation mark, if none
 		.replace(/(?<=[^\s])[-–—]/g, " -") // Add space before dash, if lacking
 		.replace(/[-–—](?=[^\s\.])/g, "- ") // Add space after dash, if lacking
 		// Turn hyphens and en dashes into em dashes
 		.replace(/[-–]/g, '—')
 		// Title fix
-		.replace(/"Krāsaina saule virs pelēkiem jumtiem"/, (x) => x.replace(/"/g, '')) // Remove the title being in quotes
-		.replace(/(?<=Zebris)( |(, )|( — )|(.[\n ]))(?=Krāsaina)/, "\n") // Replace Author, Title with Author
-									   							 //                            Title
-		// Quotation marks
-		.replace(/['`´<>“”‘’«»‟‹›„]/g, "\"")
-		.replace(/ {2,}/g, " ");
+		.replace(/"?Krāsaina saule virs pelēkiem jumtiem"?/, (x) => x.replace(/"/g, '')) // Remove the title being in quotes
+		.replace(/(?<=Zebris)((\.\s)|(,\s)|(\s—\s)|\s)+(?=Krāsaina)/, "\n") // Replace Author, Title with Author
+									   							 		  //                            Title
+		.replace(/ {2,}/g, " ")
+		// Space Quotation Marks
+		.replace(/(?<="[^\s]{0})"(?=[^"\n])/g, " \"")
+		// Space characters after closing quotation mark
+		.replace(/(?<=(\.|\?|!|,)"[^\s]{0})[^\s](?=[^"\n])/g, x => ' ' + x);
 }
